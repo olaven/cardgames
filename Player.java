d92a6b27cd2056c857c122491ff718ed10888886
olaven <olavsundfoer@gmail.com>
2017-11-06 22:31:45 +0100

    added a Player-class

---
+++ Player.java
@@ -0,0 +1,14 @@
+public class Player{
+
+  private String name;
+
+  public Player(String name){
+    setName(name);
+  }
+
+
+  //set-methods
+  public void setName(String name){
+    this.name = name;
+  }
+}
